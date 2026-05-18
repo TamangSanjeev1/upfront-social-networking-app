52d623316da1b1aaf8f7412336b75d0b876aa3c7
TamangSanjeev <sanjeevtamang1@gmail.com>
2026-05-18 11:01:55 +0900

    User profile feature TODO://fix overlapping scss file.

---
+++ upfront-front/src/app/profile/profile-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {authGuard} from "../core/guards/auth.guard";
+
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./user-profile/user-profile.component').then(m => m.UserProfileComponent),
+    canActivate: [authGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
